2030d67c539b9e5f55cdfa346609fb4d647b17ba
youmna <magdy.youmna@gmail.com>
2017-08-29 15:31:57 +0200

    authorization of edit school

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,7 +2,7 @@ class SchoolsController < ApplicationController
   
   #before_action :set_school
   before_action :authorize, only: [:index, :show, :edit, :update, :destroy]
-  before_action :authorize, except: [:create ,:new, :apply]
+  before_action :authorize, except: [:create ,:new, :apply, :edit]
   
   # GET /schools
   # GET /schools.json
